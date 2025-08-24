df9564cb980e7708aac935813e25f4a59ddaa784
brai44 <braian.urba@gmail.com>
2025-08-24 18:42:51 -0400

    RED: test para generar valor aleatorio en dado - falla como esperado

---
+++ tests/test_dado.py
@@ -0,0 +1,10 @@
+import pytest
+from tarea_dudo_TDD.src.juego.dado import Dado
+from tarea_dudo_TDD.src.servicios.generador_aleatorio import Generador_Aleatorio
+
+class TestDado:
+    def test_obtener_valor_entre_1_y_6(self):
+        dado = Dado()
+        valor = dado.lanzar()
+        assert 1<=valor<=6
+    
