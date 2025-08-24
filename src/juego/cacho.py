4cc4eabbff3844f5a5db2a81fca5131b8354d514
brai44 <braian.urba@gmail.com>
2025-08-24 19:25:50 -0400

    REFACTOR: mejora creacion cacho con list comprehension

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -2,9 +2,7 @@ from tarea_dudo_TDD.src.juego.dado import Dado
 
 class Cacho:
     def __init__(self):
-        self.dados = []
-        for i in range(5):
-            self.dados.append(Dado())
+        self.dados = [Dado() for i in range(5)]
 
     def get_dados(self):
         return self.dados
