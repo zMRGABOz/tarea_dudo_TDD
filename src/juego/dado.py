c58e93f9dd07ceb688bfa19687df555f0bc93d7d
brai44 <braian.urba@gmail.com>
2025-08-24 18:54:42 -0400

    GREEN: implementacion minima para denominar pinta del dado

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -7,4 +7,22 @@ class Dado:
 
     def lanzar(self):
         self.valor = Generador_Aleatorio().generar()
+        self.pinta = self.denominar_pinta(self.valor)
         return self.valor
+
+    def denominar_pinta(self, valor):
+        if valor==1:
+            return "As"
+        elif valor==2:
+            return "Tonto"
+        elif valor==3:
+            return "Tren"
+        elif valor==4:
+            return "Cuadra"
+        elif valor==5:
+            return "Quina"
+        elif valor==6:
+            return "Sexto"
+
+    def get_pinta(self):
+        return self.pinta
