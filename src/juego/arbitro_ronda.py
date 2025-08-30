70c5d855cdf4ca0d38ae2c3e7443ddaa9d13bed1
brai44 <braian.urba@gmail.com>
2025-08-30 15:28:37 -0400

    GREEN: implementacion minima para verificar que una duda fue correcta

---
+++ src/juego/arbitro_ronda.py
@@ -0,0 +1,16 @@
+from src.juego.cacho import Cacho
+from src.juego.contador_pintas import ContadorPintas
+
+
+class ArbitroRonda:
+    def dudar(self, cachos, apuesta_actual, obligado, indice_dudo, indice_apuesta):
+        contador = 0
+        contador_pintas = ContadorPintas()
+        apuesta_apariciones, pinta_actual = apuesta_actual
+        for cacho in cachos:
+            dados = cacho.get_dados()
+            contador += contador_pintas.contar_pinta(dados, pinta_actual, obligado)
+        if apuesta_apariciones > contador:
+            return True
+        else:
+            return False
