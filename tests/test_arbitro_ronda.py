687ca65f46d54e495706763c83c0f3603a518ff3
brai44 <braian.urba@gmail.com>
2025-08-30 16:17:10 -0400

    RED: test para verificar que calzar es incorrecto - falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -47,3 +47,17 @@ class TestArbitroRonda:
             # el metodo calzar recibe una lista de cachos, la apuesta actual, si es ronda obligado o no, y el indice del cacho que calzó,
             # devuelve True si el calce fue correcto.
             assert arbitro_ronda.calzar(cachos, (10, 2), obligado, 0) == True
+
+        def test_calzar_incorrecta(self, mocker):
+            mocker.patch("src.servicios.generador_aleatorio.random.randint", return_value=2)
+            cachos = [Cacho() for _ in range(2)]
+            for cacho in cachos:
+                cacho.agitar()
+            # Hay 10 Tontos en total
+            arbitro_ronda = ArbitroRonda()
+            obligado = False
+            # el metodo calzar recibe una lista de cachos, la apuesta actual, si es ronda obligado o no, y el indice del cacho que calzó,
+            # devuelve False si el calce fue incorrecto.
+            assert arbitro_ronda.calzar(cachos, (9, 2), obligado, 0) == False
+            assert arbitro_ronda.calzar(cachos, (11, 2), obligado, 0) == False
+            assert arbitro_ronda.calzar(cachos, (1, 3), obligado, 0) == False
