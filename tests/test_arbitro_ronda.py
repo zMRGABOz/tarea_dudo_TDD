cdb1ca186b39dd75db2693e79a4ae9faa0207682
brai44 <braian.urba@gmail.com>
2025-08-30 15:07:14 -0400

    RED: test para verificar que una duda fue correcta - falla como esperado

---
+++ tests/test_arbitro_ronda.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pytest
+from src.juego.arbitro_ronda import ArbitroRonda
+from src.juego.cacho import Cacho
+
+class TestArbitroRonda:
+    class TestArbitroRonda:
+        def test_duda_correcta(self, mocker):
+            mocker.patch("src.servicios.generador_aleatorio.random.randint", return_value=2)
+            cachos = [Cacho() for _ in range(2)]
+            for cacho in cachos:
+                cacho.agitar()
+            # Hay 10 Tontos en total
+            arbitro_ronda = ArbitroRonda()
+            obligado = False
+            # el metodo dudar recibe una lista de cachos, la apuesta actual, indice cacho que hace duda, indice cacho del que hizo la apuesta
+            # y si es ronda obligado o no, devuelve True si la duda fue correcta.
+            assert arbitro_ronda.dudar(cachos, (1, 3), obligado, 0, 1) == True
+            assert arbitro_ronda.dudar(cachos, (5, 5), obligado, 0, 1) == True
+            assert arbitro_ronda.dudar(cachos, (11, 2), obligado, 0, 1) == True
+            assert arbitro_ronda.dudar(cachos, (1, 1), obligado, 0, 1) == True
